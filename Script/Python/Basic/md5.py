06ad0825d37a37123eed7cb230a45957daf1c533
Juntaran <jacinthmail@gmail.com>
2017-10-10 11:07:59 +0800

    2017.10.10

--- Script/Python/Basic/md5.py
+++ Script/Python/Basic/md5.py
@@ -20,7 +20,7 @@ def Get_Md5_Of_File(filename):
     if not os.path.isfile(filename):
         return
     myhash = hashlib.md5()
-    f = open(filename,'rb')
+    f = open(filename, 'rb')
     while True:
         b = f.read(8096)
         if not b :
@@ -32,11 +32,11 @@ def Get_Md5_Of_File(filename):
 # 对文件夹计算 md5  递归计算目录下每个文件的md5值，写入 tmp.md5 再计算 tmp.md5 的 md5 值
 def Get_Md5_Of_Folder(dir):
   MD5File = "tmp.md5"
-  outfile = open(MD5File,'w')
+  outfile = open(MD5File, 'w')
   for root, subdirs, files in os.walk(dir):
     for file in files:
-      filefullpath = os.path.join(root,file)
-      filerelpath = os.path.relpath(filefullpath,dir)
+      filefullpath = os.path.join(root, file)
+      filerelpath = os.path.relpath(filefullpath, dir)
       md5 = Get_Md5_Of_File(filefullpath)
       outfile.write(md5)
   outfile.close()
