60ef53cc70996d756cd8c05b3c7a667663299406
Juntaran <jacinthmail@gmail.com>
2017-12-13 21:08:59 +0800

    2017.12.13

--- Script/Python/Interests/weiboPicCrawler/weiboPicCrawler.py
+++ Script/Python/Interests/weiboPicCrawler/weiboPicCrawler.py
@@ -1,4 +1,4 @@
-# coding=utf-8
+# encoding=utf-8
 
 '''
     Author: Juntaran
@@ -11,6 +11,7 @@
 import os
 import requests
 import json
+import urllib
 
 NICKNAMES_FILE = 'weibo_nicknames.txt'
 HEADERS = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'}
@@ -26,23 +27,52 @@ def readNickname():
 
 # 把 nickname 转化为 id
 def nicknameToContainerid(nickname):
+    nickname = urllib.parse.quote(nickname)
     url = "http://m.weibo.com/n/{}".format(nickname)
-    resp = getUrl(url, allow_redirects=False)
-    cid = resp.headers['Location'][27:]
-    return "107603" + cid
+
+    print("url: ", url)
+
+    resp = getUrl(url)
+
+    # http://m.weibo.com/n/%EF%BB%BF%E7%A1%AC%E5%8D%A7%E5%AE%A2
+    # http://m.weibo.com/n/%E7%A1%AC%E5%8D%A7%E5%AE%A2
+    print("resp.headers: ", resp.headers)
+    print("resp.text: ", resp.text)
+
+    # 跳转后的url
+    redirect_url = resp.url
+    # 状态码
+    code = resp.status_code
+
+    print("redirect_url:", redirect_url)
+    print("code:", code)
+
+    new_url = redirect_url.replace("%EF%BB%BF", "")
+    print("new_url:", new_url)
+
+    resp = getUrl(new_url)
+    print("resp.url: ", resp.url)
+
+    cid = resp.url.split("/")[-1]
+    print("cid: ", cid)
+    # return "107603" + cid
+    return cid[6:]
+
 
 # 根据 url 发起请求
 def getUrl(url, stream=False, allow_redirects=True, timeout=10):
     print(url)
-    return requests.get(url, headers=HEADERS, allow_redirects=allow_redirects, timeout=timeout)
+    return requests.get(url, headers=HEADERS, allow_redirects=True, timeout=timeout)
 
 # 根据 containerid 和 page 把所有图片地址提取出来
-def getUrls(containerid, page):
-    url = "https://m.weibo.cn/api/container/getIndex?count=25&containerid={}&page={}".format(containerid, page)
-    resp_text = getUrl(url=url).text
+def getImageUrls(cid, page):
+    # url = "https://m.weibo.cn/api/container/getIndex?count=25&containerid={}&page={}".format(containerid, page)
+    url = "https://m.weibo.cn/api/container/getIndex?type=uid&value={}&containerid={}&page={}".format(cid, "107603" + cid, page)
+    print("url: ", url)
+    resp_text = getUrl(url).text
     json_data = json.loads(resp_text)
-    # print(json_data)
-    cards = json_data['cards']
+    print("resp_text:", resp_text)
+    cards = json_data['data']['cards']
     # print(cards)
     photos = []
     if not cards:
@@ -89,7 +119,7 @@ def userRun(nickname):
     check = True
     while check:
         page += 1
-        urls = getUrls(containerid=cid, page=page)
+        urls = getImageUrls(cid=cid, page=page)
         check = bool(urls)
         if check:
             print(check)
